6ea5adc35ac1176bbbee7f446b57d832077bb9fc
Kenaz79 <blessedkentacion@gmail.com>
2025-06-14 14:02:14 +0300

    changed urls

--- backend/store/urls.py
+++ backend/store/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ProductListCreateView, ProductDetailView
+from .views import LoginView, ProductListView, RegisterView
 
 urlpatterns = [
-    path('products/', ProductListCreateView.as_view(), name='product-list'),
-    path('products/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
+    path('products/', ProductListView.as_view(), name='product-list'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
 ]
