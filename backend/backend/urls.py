30d94d1da073872dcca1c39b6d9078fdc59fc179
Kenaz79 <blessedkentacion@gmail.com>
2025-06-14 14:01:33 +0300

    updated urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,13 +15,27 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from store.views import ProductListView
+from store.views import LoginView
+
 from django.urls import path, include
 from django.conf.urls.static import static
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 
 from backend import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    path('api/products/', ProductListView.as_view(), name='product-list'),
     path('api/', include('store.urls')),
+     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/login/', LoginView.as_view(), name='login'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
