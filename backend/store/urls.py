5b63f09f6541e69e8e454e74eb675fc53fbbaf4b
Kenaz79 <blessedkentacion@gmail.com>
2025-06-15 13:26:55 +0300

    Changes were made

--- backend/store/urls.py
+++ backend/store/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 from . import views
 from .views import LoginView, ProductListView, RegisterView
+from .views import SendOTPView
+from rest_framework.views import APIView
+
 
 urlpatterns = [
     path('products/', ProductListView.as_view(), name='product-list'),
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
-      path('send-otp/', views.send_otp_email, name='send_otp'),
+    path('send-otp/', SendOTPView.as_view(), name='send_otp'),
     path('verify-otp/', views.verify_otp_view, name='verify_otp'),
     path('main/', views.main_page, name='main_page'),
 ]
