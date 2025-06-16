77c3cf9bc560c19785bab4597e3b01ecebe99995
Kenaz79 <blessedkentacion@gmail.com>
2025-06-16 23:48:48 +0300

    updated serializers

--- backend/store/serializers.py
+++ backend/store/serializers.py
@@ -3,19 +3,25 @@ from django.contrib.auth.models import User
 from .models import Product
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True, min_length=6)
+    confirm_password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
+        fields = ['username', 'email', 'password', 'confirm_password']
+
+    def validate(self, data):
+        if data['password'] != data['confirm_password']:
+            raise serializers.ValidationError({"password": "Passwords do not match."})
+        return data
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+        validated_data.pop('confirm_password')  # Remove before user creation
+        return User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password']
         )
-        return user
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
