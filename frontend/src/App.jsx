e4a9d5a9aed46dbcfcc39e4937c187d46010bb59
Kenaz79 <blessedkentacion@gmail.com>
2025-06-17 14:26:48 +0300

    Added more routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
 import Register from './pages/Register';
+import EditProfile from './components/EditProfile';
+import ChangePassword from './components/ChangePassword';
+import SecurityPrivacy from './components/SecurityPrivacy';
 import Login from './pages/Login';
 import Home from './pages/Home';
+import Products from './pages/Products';
 import OtpPage from './pages/OtpPage';
 import Settings from './pages/Settings'; // Import Settings page if needed
 
@@ -10,9 +14,13 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/settings" element={<Settings />} />
+      <Route path="/products" element={<Products />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/edit-profile" element={<EditProfile />} />
+      <Route path="/change-password" element={<ChangePassword />} />
+      <Route path="/security-privacy" element={<SecurityPrivacy />} />
       <Route path="/login" element={<Login />} />
-        <Route path="/otp" element={<OtpPage />} /> {/* ✅ Add this line */}
+      <Route path="/otp" element={<OtpPage />} /> {/* ✅ Add this line */}
     </Routes>
   );
 }
