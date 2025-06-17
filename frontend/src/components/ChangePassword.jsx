a4b9befd44a091f59e1979d9d740eba05af6b3d5
Kenaz79 <blessedkentacion@gmail.com>
2025-06-17 14:27:11 +0300

    Added a change password feature

---
+++ frontend/src/components/ChangePassword.jsx
@@ -0,0 +1,8 @@
+export default function ChangePassword() {
+  return (
+    <div className="page">
+      <h2>Change Password</h2>
+      <p>Enter old and new password fields here.</p>
+    </div>
+  );
+}
