ebc8dbc30189e102541af72c0fb6e2cbc122754e
Kenaz79 <blessedkentacion@gmail.com>
2025-06-17 14:27:30 +0300

    added an edit profile feature

---
+++ frontend/src/components/EditProfile.jsx
@@ -0,0 +1,8 @@
+export default function EditProfile() {
+  return (
+    <div className="page">
+      <h2>Edit Profile</h2>
+      <p>This is where you can change name, email, etc.</p>
+    </div>
+  );
+}
